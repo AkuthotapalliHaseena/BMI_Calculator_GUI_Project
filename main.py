eebf0499eb2440e2b4a5b4c70caf91749e77f956
Haseena <akuthotapallihaseena@gmail.com>
2026-01-09 11:41:04 +0530

    main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from ui.bmi_gui import BMIGUI
 
 if __name__ == "__main__":
     root = tk.Tk()
+    root.geometry("500x700")
+    root.configure(bg="pink")
     app = BMIGUI(root)
     root.mainloop()
-6
